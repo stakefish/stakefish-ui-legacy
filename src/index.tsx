3496f08a3af5c86a29b3b8d5fbd9bb9f1d4f2183
Balaj Marius <balajmarius@gmail.com>
2021-04-27 12:59:46 +0300

    cleanup

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <h2>hello world</h2>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
