8aab92d94b2620440163ffccee924c878462b85a
Balaj Marius <balajmarius@gmail.com>
2021-05-11 15:25:44 +0300

    [colors] removed contrastText

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,28 +2,24 @@ const colors = {
   red: {
     main: "#FF5959",
     dark: "#F04646",
-    contrastText: "#FFFFFF",
   },
   green: {
     bright: "#82D318",
     light: "#6BBD00",
     main: "#0BA012",
     dark: "#008E07",
-    contrastText: "#FFFFFF",
   },
   black: {
     bright: "#7D8499",
     light: "#697087",
     main: "#313540",
     dark: "#00030E",
-    contrastText: "#FFFFFF",
   },
   gray: {
     bright: "#FAFBFC",
     light: "#F5F5F7",
     main: "#E1E3E8",
     dark: "#BBBEC7",
-    contrastText: "#00030E",
   },
 };
 
