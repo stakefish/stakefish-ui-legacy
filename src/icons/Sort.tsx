cf3fadf64550099a45575a40049534e2efc36ca5
vince19972 <vince19972@gmail.com>
2021-05-12 15:57:05 -0400

    [icons] add icon component and files

---
+++ src/icons/Sort.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Sort = () => {
+  return (
+    <svg viewBox="0 0 32 32">
+      <path d="M9.6 6.4h-3.2v11.2h-4.8l6.4 8 6.4-8h-4.8zM24 6.4l-6.4 8h4.8v11.2h3.2v-11.2h4.8z"></path>
+    </svg>
+  )
+}
+
+export default Sort
---
+++ src/icons/SortUp.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SortUp = () => {
+  return (
+    <svg viewBox="0 0 19 32">
+      <path d="M9.6 6.4l-6.4 8h4.8v11.2h3.2v-11.2h4.8z"></path>
+    </svg>
+  )
+}
+
+export default SortUp
