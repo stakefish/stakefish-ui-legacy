172f574d177b2ba52d7c37eabe9294ed8b9e54db
vince19972 <vince19972@gmail.com>
2021-05-13 11:09:33 -0400

    [input] add adornment settings and story

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,12 +3,12 @@ import MuiBox from "@material-ui/core/Box";
 import MuiInput from "@material-ui/core/Input";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 
+import Icon, { IconProps } from "./Icon";
 import Typography from "./Typography";
 import MuiFormHelperText from "./FormHelperText";
-
 export interface InputProps {
-  endAdornment?: ReactNode;
-  startAdornment?: ReactNode;
+  startAdornmentProps?: IconProps;
+  endAdornmentProps?: IconProps;
   placeholder?: string;
   textSize?: "big" | "medium";
   label?: string;
@@ -23,10 +23,13 @@ const useStyles = makeStyles((theme: Theme) => ({
       lineHeight: 1,
     },
   },
-  input: (props: { textSize: string }) => ({
-    padding: theme.spacing(1.5, 3),
+  input: (props: { textSize: string; narrowEndPadding: boolean }) => ({
+    padding: theme.spacing(1.5, props.narrowEndPadding ? 2.5 : 3, 1.5, 3),
     lineHeight: 1,
     fontSize: props.textSize === "big" ? theme.typography.fontSizeLg : theme.typography.fontSizeM,
+    "& .icon:first-of-type": {
+      marginRight: theme.spacing(1.5),
+    },
   }),
 }));
 
@@ -34,13 +37,16 @@ const Input: React.FC<InputProps> = ({
   placeholder,
   textSize = "big",
   label,
-  startAdornment,
-  endAdornment,
-  error = false,
   helperText,
+  startAdornmentProps = undefined,
+  endAdornmentProps = undefined,
+  error = false,
   disabled = false,
 }: InputProps) => {
-  const classes = useStyles({ textSize });
+  const classes = useStyles({ textSize, narrowEndPadding: !!endAdornmentProps });
+
+  const StartAdornment = startAdornmentProps && <Icon size="sm" {...startAdornmentProps} />;
+  const EndAdornment = endAdornmentProps && <Icon size="sm" {...endAdornmentProps} />;
 
   return (
     <MuiBox display="inline-flex" flexDirection="column" alignItems="flex-start" className={`${classes.box}`}>
@@ -53,8 +59,8 @@ const Input: React.FC<InputProps> = ({
       )}
       <MuiInput
         className={`${classes.input}`}
-        startAdornment={startAdornment}
-        endAdornment={endAdornment}
+        startAdornment={StartAdornment}
+        endAdornment={EndAdornment}
         placeholder={placeholder}
         inputProps={{ style: { padding: 0 } }}
         disabled={disabled}
--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,6 +1,19 @@
 import { Story, Meta } from "@storybook/react";
-
+import { iconList, iconColors, IconProps } from "../components/Icon";
+import { IconKeys, IconColors } from "../../definitions/icon";
 import { Input, InputProps } from "../";
+
+const iconNameKeys = [...Object.keys(iconList), ""];
+const iconColorKeys = [...Object.keys(iconColors), ""];
+
+interface InputStoryProps extends InputProps {
+  startAdornmentKey?: keyof IconKeys;
+  startAdornmentColor?: keyof IconColors;
+  startAdornmentRotation?: number;
+  endAdornmentKey?: keyof IconKeys;
+  endAdornmentColor?: keyof IconColors;
+  endAdornmentRotation?: number;
+}
 
 const argTypes = {
   textSize: {
@@ -16,10 +29,87 @@ const argTypes = {
       defaultValue: { summary: "big" },
     },
   },
+  /**
+   * startAdornment controls
+   */
+  startAdornmentProps: { table: { disable: true } },
+  startAdornmentKey: {
+    name: "startAdornmentKey",
+    description: "The key name of icon component.",
+    control: {
+      type: "select",
+      options: iconNameKeys,
+    },
+    table: { category: "startAdornment" },
+  },
+  startAdornmentColor: {
+    name: "startAdornmentColor",
+    description: "The color icon component.",
+    control: {
+      type: "select",
+      options: iconColorKeys,
+    },
+    table: { category: "startAdornment" },
+  },
+  startAdornmentRotation: {
+    name: "startAdornmentRotation",
+    description: "The rotation value of icon component.",
+    control: {
+      type: "number",
+      default: 0,
+    },
+    table: { category: "startAdornment" },
+  },
+  /**
+   * endAdornment controls
+   */
+  endAdornmentProps: { table: { disable: true } },
+  endAdornmentKey: {
+    name: "endAdornmentKey",
+    description: "The key name of icon component.",
+    control: {
+      type: "select",
+      options: iconNameKeys,
+    },
+    table: { category: "endAdornment" },
+  },
+  endAdornmentColor: {
+    name: "endAdornmentColor",
+    description: "The color icon component.",
+    control: {
+      type: "select",
+      options: iconColorKeys,
+    },
+    table: { category: "endAdornment" },
+  },
+  endAdornmentRotation: {
+    name: "endAdornmentRotation",
+    description: "The rotation value of icon component.",
+    control: {
+      type: "number",
+      default: 0,
+    },
+    table: { category: "endAdornment" },
+  },
 };
 
-const Template: Story<InputProps> = (args: InputProps) => {
-  return <Input {...args} />;
+const Template: Story<InputProps> = (args: InputStoryProps) => {
+  const startAdornmentProps: IconProps | undefined = args.startAdornmentKey?.length
+    ? {
+        iconKey: args.startAdornmentKey,
+        color: args.startAdornmentColor,
+        rotate: args.startAdornmentRotation,
+      }
+    : undefined;
+  const endAdornmentProps: IconProps | undefined = args.endAdornmentKey?.length
+    ? {
+        iconKey: args.endAdornmentKey,
+        color: args.endAdornmentColor,
+        rotate: args.endAdornmentRotation,
+      }
+    : undefined;
+
+  return <Input {...args} startAdornmentProps={startAdornmentProps} endAdornmentProps={endAdornmentProps} />;
 };
 
 export const Default = Template.bind({});
