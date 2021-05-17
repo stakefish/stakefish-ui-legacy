30e1abeb342d8632531208e0675a87244091914f
vince19972 <vince19972@gmail.com>
2021-05-17 16:51:38 -0400

    [textField] add forwardRef wrapper

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import FormControl, { FormControlProps as MuiFormControlProps } from "@material-ui/core/FormControl";
 
@@ -24,30 +24,33 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-const TextField: React.FC<TextFieldProps> = ({ id, label, helperText, error, inputProps, ...props }) => {
-  const classes = useStyles();
+const TextField: React.FC<TextFieldProps> = forwardRef(
+  ({ id, label, helperText, error, inputProps, ...props }, ref) => {
+    const classes = useStyles();
 
-  const formControlProps = {
-    onChange: props.onChange,
-    onBlur: props.onBlur,
-    required: props.required,
-  };
+    const formControlProps = {
+      ref,
+      onChange: props.onChange,
+      onBlur: props.onBlur,
+      required: props.required,
+    };
 
-  return (
-    <FormControl error={!!error} classes={classes} {...formControlProps}>
-      {label && <InputLabel htmlFor={id}>{label}</InputLabel>}
+    return (
+      <FormControl error={!!error} classes={classes} {...formControlProps}>
+        {label && <InputLabel htmlFor={id}>{label}</InputLabel>}
 
-      <Input {...inputProps} id={id} aria-describedby={`${id}-text`} />
+        <Input {...inputProps} id={id} aria-describedby={`${id}-text`} />
 
-      {helperText && error && (
-        <FormHelperText
-          id={`${id}-text`}
-          text={helperText}
-          startAdornment={<Icon iconKey="info-circle" color="contrastPrimary" size="xs" />}
-        />
-      )}
-    </FormControl>
-  );
-};
+        {helperText && error && (
+          <FormHelperText
+            id={`${id}-text`}
+            text={helperText}
+            startAdornment={<Icon iconKey="info-circle" color="contrastPrimary" size="xs" />}
+          />
+        )}
+      </FormControl>
+    );
+  }
+);
 
 export default TextField;
